70ab794b86f0a849f94a60321e0959d75b814c48
Jakub Kuczkowiak <jakubkuczkowiak@gmail.com>
2018-04-03 23:22:47 +0200

    storms: Implemented rows and columns constraints

--- P3/storms.py
+++ P3/storms.py
@@ -8,11 +8,52 @@ def V(i, j):
 def domains(Vs):
     return [ q + ' in 0..1' for q in Vs ]
 
-def rowSum(index, rows):
-    return []
+def row(index, columnsNumber, rowSum):
+    result = ""
+    for j in range(0, columnsNumber):
+        if (j != columnsNumber - 1):
+            result += V(index, j) + '+'
+        else:
+            result += V(index, j)
+    
+    result += " #= "
+    result += str(rowSum)
+    return result
 
-def columnSum(index, column):
-    return []
+def gen_rows(rowsNumber, columnsNumber, rowsDescription):
+    result = ""
+    for i in range(0, rowsNumber):
+        result += row(i, columnsNumber, rowsDescription[i])
+        result += ","
+    return result
+    
+def column(index, rowsNumber, columnSum):
+    result = ""
+    for i in range(0, rowsNumber):
+        if (i != rowsNumber - 1):
+            result += V(i, index) + '+'
+        else:
+            result += V(i, index)
+    
+    result += " #= "
+    result += str(columnSum)
+    return result
+
+def gen_columns(rowsNumber, columnsNumber, columnsDescription):
+    result = ""
+    for j in range(0, columnsNumber):
+        result += column(j, rowsNumber, columnsDescription[j])
+        result += ","
+    return result
+
+def rectangles():
+    return
+
+def atleast2x2():
+    return
+
+def notConnectedWithCorners():
+    return
 
 def print_constraints(Cs, indent, d):
     position = indent
@@ -29,8 +70,19 @@ def print_listDeclaration(Vs):
     for i,j,val in Vs:
         file.write('%s #= %d, ' % (V(i,j), val))
 
-def formatOutput():
-    file.write("")
+def formatOutput(columnsNumber):
+    heads = ""
+    for i in range(0, columnsNumber):
+        heads += ("H" + str(i))
+        if (i != columnsNumber - 1):
+            heads += ", "
+
+    file.write("formatOutput([]) :- !.")
+    file.write("\n")
+    file.write("formatOutput(X) :-")
+    file.write("[" + heads + "|T] = X,")
+    file.write("write([" + heads + "]), write('\\n'),")
+    file.write("formatOutput(T).")
 
 def generate(input):
     (rows, columns, listDecl) = input
@@ -39,6 +91,8 @@ def generate(input):
 
     variables = [ V(i,j) for i in range(rowsNumber) for j in range(columnsNumber)]
     constraints = domains(variables)
+    rowsStr = gen_rows(rowsNumber, columnsNumber, rows)
+    columnsStr = gen_columns(rowsNumber, columnsNumber, columns)
 
     file.write(':- use_module(library(clpfd)).')
     file.write('\n')
@@ -48,11 +102,16 @@ def generate(input):
     print_listDeclaration(listDecl)
 
     file.write('\n')
+    file.write(rowsStr)
+    file.write('\n')
+    file.write(columnsStr)
+
+    file.write('\n')
     file.write('    labeling([ff], [' +  ', '.join(variables) + ']).') 
     file.write('\n')
 
     file.write('\n')
-    formatOutput()
+    formatOutput(columnsNumber)
     file.write('\n')
 
     file.write(':- solve(X), formatOutput(X), nl.') 
