48616be57ae6c52493e95f249cb2b5df42d8d43a
Jakub Kuczkowiak <jakubkuczkowiak@gmail.com>
2018-04-03 17:44:28 +0200

    storms: initiallist implementation

--- P3/storms.py
+++ P3/storms.py
@@ -25,19 +25,37 @@ def print_constraints(Cs, indent, d):
             file.write('\n')
             file.write( (indent - 1) * ' ')
 
+def print_listDeclaration(Vs):
+    for i,j,val in Vs:
+        file.write('%s #= %d, ' % (V(i,j), val))
+
+def formatOutput():
+    file.write("")
+
 def generate(input):
     (rows, columns, listDecl) = input
     rowsNumber = len(rows)
     columnsNumber = len(columns)
 
     variables = [ V(i,j) for i in range(rowsNumber) for j in range(columnsNumber)]
-    domain = domains(variables)
+    constraints = domains(variables)
 
     file.write(':- use_module(library(clpfd)).')
     file.write('\n')
     file.write('solve([' + ', '.join(variables) + ']) :- ')
 
-    print_constraints(domain, 4, 70)
+    print_constraints(constraints, 4, 70)
+    print_listDeclaration(listDecl)
+
+    file.write('\n')
+    file.write('    labeling([ff], [' +  ', '.join(variables) + ']).') 
+    file.write('\n')
+
+    file.write('\n')
+    formatOutput()
+    file.write('\n')
+
+    file.write(':- solve(X), formatOutput(X), nl.') 
 
 input = ([4, 8, 6, 2, 5, 8, 6, 2, 6, 6],
     [4, 7, 5, 2, 3, 5, 7, 2, 6, 8, 2, 2],
