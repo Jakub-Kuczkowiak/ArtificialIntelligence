a2450863f31377422773557f05493a4168788196
Jakub Kuczkowiak <jakubkuczkowiak@gmail.com>
2018-04-13 23:22:48 +0200

    NonogramsDeductor: working, but without optimizations yet

--- P3/NonogramsDeductor/NonogramsDeductor/main.cpp
+++ P3/NonogramsDeductor/NonogramsDeductor/main.cpp
@@ -19,6 +19,9 @@ typedef tuple<int, int, int> Change;
 //	UNSET
 //};
 
+vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, int rowId, int elem, int index);
+vector< vector<int> > generateColumnPossibilities(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, int columnId, int elem, int index);
+
 bool deduceRow(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, int rowId, vector<Change>& changes) {
 	vector< vector<int> > possibilities = generateRowPossibilities(rows, columns, picture, rowId, 0, 0);
 	if (possibilities.empty()) return false; // couldn't deduce due to some previous backtracking
@@ -38,6 +41,38 @@ bool deduceRow(vector< vector<int> >& rows, vector< vector<int> >& columns, vect
 	for (int i = 0; i < mask.size(); i++) {
 		if (mask[i] != -1) {
 			Change change;
+			std::get<0>(change) = rowId;
+			std::get<1>(change) = i;
+			std::get<2>(change) = mask[i];
+			changes.push_back(change);
+		}
+	}
+
+	return true;
+}
+
+bool deduceColumn(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, int columnId, vector<Change>& changes) {
+	vector< vector<int> > possibilities = generateColumnPossibilities(rows, columns, picture, columnId, 0, 0);
+	if (possibilities.empty()) return false; // couldn't deduce due to some previous backtracking
+
+	vector<int> mask = possibilities[0];
+
+	// we build mask
+	for (auto& possibility : possibilities) {
+		for (int i = 0; i < possibility.size(); i++) {
+			if (mask[i] != -1) {
+				if (mask[i] == !possibility[i])
+					mask[i] = -1;
+			}
+		}
+	}
+
+	for (int i = 0; i < mask.size(); i++) {
+		if (mask[i] != -1) {
+			Change change;
+			std::get<0>(change) = i;
+			std::get<1>(change) = columnId;
+			std::get<2>(change) = mask[i];
 			changes.push_back(change);
 		}
 	}
@@ -50,8 +85,10 @@ vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vect
 	int elemSize = rows[rowId][elem];
 	int elemNumber = rows[rowId].size();
 
-	for (int i = index; i < m - elemSize; i++) {
+	for (int i = index; i <= m - elemSize; i++) {
 		// check if we can place block of ones starting at index 'i'.
+		if (i > 0 && picture[rowId][i - 1] == 1) break; // if previous is 1, we can't start placing here and after
+
 		bool canPlace = true;
 		for (int j = i; j < i + elemSize; j++) {
 			if (picture[rowId][j] == 0) {
@@ -60,10 +97,12 @@ vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vect
 			}
 		}
 
+		if (!canPlace) continue;
+
 		// is it the last element?
 		if (elem == elemNumber - 1) { // base case
 			// we must check if all fields are zeros
-			for (int j = i + elemSize + 1; j < m; j++) {
+			for (int j = i + elemSize; j < m; j++) {
 				if (picture[rowId][j] == 1) {
 					canPlace = false;
 					break;
@@ -71,22 +110,24 @@ vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vect
 			}
 
 			if (canPlace) {
-				vector<int> v = vector<int>(0, index - i); // fill the preblock
+				vector<int> v = vector<int>(i - index, 0); // fill the preblock
 
 				// fill our block with 1s
 				for (int j = 0; j < elemSize; j++)
 					v.push_back(1);
 
 				// fill the rest with 0s
-				for (int j = i + elemSize + 1; j < m; j++)
+				for (int j = i + elemSize; j < m; j++)
 					v.push_back(0);
 
 				result.push_back(v);
 			}
 		}
 		else { // if not the last element: induction step
+			if (i + elemSize >= m || picture[rowId][i + elemSize] == 1) canPlace = false;
+
 			if (canPlace) {
-				vector<int> v = vector<int>(0, index - i); // fill the preblock
+				vector<int> v = vector<int>(i - index, 0); // fill the preblock
 
 				// fill our block with 1s
 				for (int j = 0; j < elemSize; j++)
@@ -94,7 +135,7 @@ vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vect
 
 				v.push_back(0); // add one zero after block
 
-				vector< vector<int> > lastPossibilities = generateRowPossibilities(rows, columns, picture, rowId, elem + 1, index + elemSize + 1);
+				vector< vector<int> > lastPossibilities = generateRowPossibilities(rows, columns, picture, rowId, elem + 1, i + elemSize + 1);
 				for (vector<int> possibility : lastPossibilities) {
 					vector<int> r = v;
 					r.insert(r.end(), possibility.begin(), possibility.end());
@@ -107,9 +148,193 @@ vector< vector<int> > generateRowPossibilities(vector< vector<int> >& rows, vect
 	return result;
 }
 
+vector< vector<int> > generateColumnPossibilities(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, int columnId, int elem, int index) {
+	vector< vector<int> > result;
+	int elemSize = columns[columnId][elem];
+	int elemNumber = columns[columnId].size();
 
-void deduce(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture) {
+	for (int i = index; i <= n - elemSize; i++) {
+		// check if we can place block of ones starting at index 'i'.
+		if (i > 0 && picture[i - 1][columnId] == 1) break; // if previous is 1, we can't start placing here and after
 
+		bool canPlace = true;
+		for (int j = i; j < i + elemSize; j++) {
+			if (picture[j][columnId] == 0) {
+				canPlace = false;
+				break;
+			}
+		}
+
+		if (!canPlace) continue;
+
+		// is it the last element?
+		if (elem == elemNumber - 1) { // base case
+									  // we must check if all fields are zeros
+			for (int j = i + elemSize; j < n; j++) {
+				if (picture[j][columnId] == 1) {
+					canPlace = false;
+					break;
+				}
+			}
+
+			if (canPlace) {
+				vector<int> v = vector<int>(i - index, 0); // fill the preblock
+
+														   // fill our block with 1s
+				for (int j = 0; j < elemSize; j++)
+					v.push_back(1);
+
+				// fill the rest with 0s
+				for (int j = i + elemSize; j < n; j++)
+					v.push_back(0);
+
+				result.push_back(v);
+			}
+		}
+		else { // if not the last element: induction step
+			if (i + elemSize >= n || picture[i + elemSize][columnId] == 1) canPlace = false;
+
+			if (canPlace) {
+				vector<int> v = vector<int>(i - index, 0); // fill the preblock
+
+				// fill our block with 1s
+				for (int j = 0; j < elemSize; j++)
+					v.push_back(1);
+
+				v.push_back(0); // add one zero after block
+
+				vector< vector<int> > lastPossibilities = generateColumnPossibilities(rows, columns, picture, columnId, elem + 1, i + elemSize + 1);
+				for (vector<int> possibility : lastPossibilities) {
+					vector<int> r = v;
+					r.insert(r.end(), possibility.begin(), possibility.end());
+					result.push_back(r);
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+void applyChanges(vector< vector<int> >& picture, vector<Change>& changes) {
+	for (Change& change : changes) {
+		picture[std::get<0>(change)][std::get<1>(change)] = std::get<2>(change);
+	}
+}
+
+void rollbackChanges(vector< vector<int> >& picture, vector<Change>& changes) {
+	for (Change& change : changes) {
+		picture[std::get<0>(change)][std::get<1>(change)] = -1;
+	}
+}
+
+bool deduce(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture, vector<Change>& changes) {
+	bool isConflict = false;
+	for (int i = 0; i < rows.size(); i++) {
+		if (!deduceRow(rows, columns, picture, i, changes)) {
+			return false;
+		}
+	}
+
+	applyChanges(picture, changes); // in the 2nd version we will need to rollback changes!
+
+	for (int j = 0; j < columns.size(); j++) {
+		if (!deduceColumn(rows, columns, picture, j, changes)) {
+			return false;
+		}
+	}
+
+	applyChanges(picture, changes);
+
+	return true;
+}
+
+
+void solve(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture) {
+	vector<Change> changes;
+	if (deduce(rows, columns, picture, changes)) {
+		//applyChanges(picture, changes);
+	}
+}
+
+bool isRowCorrect(vector< vector<int> >& rows, int rowIndex, vector< vector<int> >& picture) {
+	vector<int> blocks;
+	for (int i = 0; i < m; i++) {
+		if (picture[rowIndex][i] == -1) return false;
+
+		if (picture[rowIndex][i] == 1) {
+			int count = 1;
+			int j;
+			for (j = i + 1; j < m; j++) {
+				if (picture[rowIndex][j] == -1) return false;
+
+				if (picture[rowIndex][j] == 0) {
+					break;
+				}
+
+				count++;
+			}
+			i = j;
+
+			blocks.push_back(count);
+		}
+	}
+
+	return (blocks == rows[rowIndex]);
+}
+
+bool isColumnCorrect(vector< vector<int> >& columns, int columnIndex, vector< vector<int> >& picture) {
+	vector<int> blocks;
+	for (int i = 0; i < n; i++) {
+		if (picture[i][columnIndex] == -1) return false;
+
+		if (picture[i][columnIndex] == 1) {
+			int count = 1;
+			int j;
+			for (j = i + 1; j < n; j++) {
+				if (picture[j][columnIndex] == -1) return false;
+
+				if (picture[j][columnIndex] == 0) {
+					break;
+				}
+
+				count++;
+			}
+			i = j;
+
+			blocks.push_back(count);
+		}
+	}
+
+	return (blocks == columns[columnIndex]);
+}
+
+bool isPictureCorrect(vector< vector<int> >& rows, vector< vector<int> >& columns, vector< vector<int> >& picture) {
+	for (int i = 0; i < n; i++) {
+		if (!isRowCorrect(rows, i, picture))
+			return false;
+	}
+
+	for (int j = 0; j < m; j++) {
+		if (!isColumnCorrect(columns, j, picture))
+			return false;
+	}
+
+	return true;
+}
+
+void printPicture(vector< vector<int> > picture) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (picture[i][j] == 0) cout << ".";
+			else cout << "#";
+			//cout << picture[i][j];
+		}
+
+		cout << endl;
+	}
+
+	//cout << endl;
 }
 
 int main()
@@ -176,7 +401,10 @@ int main()
 		}
 
 		while (true) {
-			deduce(rows, columns, picture);
+			solve(rows, columns, picture);
+			if (isPictureCorrect(rows, columns, picture)) {
+				break;
+			}
 		}
 
 		//while (!solve(rows, columns, picture)) {
@@ -193,7 +421,7 @@ int main()
 
 		clock_t end = clock();
 
-		//printPicture(picture);
+		printPicture(picture);
 		sumTime += double(end - begin);
 		cout << "time elapsed: " << double(end - begin) << endl << endl;
 	}
